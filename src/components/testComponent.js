f3c7f705c6c7918761de1ddb8abf8d4e90da3b92
Huda Qureshi <hudaqureshi@Hudas-MBP.attlocal.net>
2023-04-19 17:13:35 -0400

    Add testComponent to play around with components

---
+++ src/components/testComponent.js
@@ -0,0 +1,10 @@
+import React,{useState} from "react";
+import "./testComponent.css"; 
+
+function TestComponent (props){ 
+
+    return (<div className='testComponent'> Hello World! I'm a test component. </div>)
+
+}
+
+export default TestComponent; 
